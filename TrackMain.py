d4590b8f2234d27e462efe23ab1650e5752fbde3
alatrizte <iruppen72@yahoo.es>
2023-07-20 13:11:38 +0200

    first commit

---
+++ TrackMain.py
@@ -0,0 +1,36 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+tracker = cv2.legacy.TrackerMOSSE_create()
+
+success, img = cap.read()
+
+bbox = cv2.selectROI("tracking", img, False)
+tracker.init(img, bbox)
+
+def drawBox(img, bbox):
+    x,y,w,h = int(bbox[0]),int(bbox[1]), int(bbox[2]), int(bbox[3])
+    cv2.rectangle(img, (x,y), ((x+w),(y+h)), (255, 0, 0), 1, 1)
+    cv2.putText(img,"tracking",(75,75),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),1)
+
+while True:
+    timer = cv2.getTickCount()
+    success, img = cap.read()
+
+    success, bbox = tracker.update(img)
+
+    if success:
+        drawBox(img, bbox)
+    else:
+        cv2.putText(img,"lost",(75,75),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.7,(0,0,255),1)
+
+    
+    fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
+
+    cv2.putText(img,str(int(fps)),(75,50),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),1)
+
+    cv2.imshow("tracking", img)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
